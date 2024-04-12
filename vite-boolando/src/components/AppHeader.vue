ac2417bf4651f55da0f4472f4e535275bfdea04c
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-04-12 14:50:20 +0200

    add

---
+++ vite-boolando/src/components/AppHeader.vue
@@ -0,0 +1,41 @@
+<template>
+    <header>
+            <nav class="header-bar container">
+                <div>
+                    <p>Donna Uomo Bambini</p>
+                </div>
+                <div>
+                    <img src="./img/boolean-logo.png ." alt="">
+                </div>
+                <div>
+                    <p>&#127900;&#127900;&#127900;</p>
+                </div>
+            </nav>
+        </header>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss">
+.header-bar{
+    display: flex;
+    justify-content: space-between;
+    line-height: 50px;
+    background-color: rgb(255, 115, 0);
+    padding: 0 65px;
+    color: white;
+    align-items: center;
+    margin-bottom: 40px;
+}
+.header-bar p{
+    font-size: 15px;
+}
+.header-bar img{
+    display: flex;
+    max-height: 30px;
+}
+</style>
