7b0baa34529f3a6ecec4ff07b5971eaec7a6224e
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-04-15 16:37:24 +0200

    ADD

---
+++ recap-components/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Ciaooo</h1>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    getRandomCard() { 
+      axios.get('https://api.magicthegathering.io/v1/cards?page=1&pageSize=1')
+       .then(res => {
+          console.log(res)
+        })
+    }
+  },
+  mounted() {
+    this.getRandomCard()
+  },
+}
+</script>
+
+<style lang="scss"></style>
